7058800386c80fbc3df729bb55e3003b3fd6fe40
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2022-05-25 00:21:37 -0400

    created a state in Quotescontainer component and passing it as props to QuotesList and did the conditional rendering to display the length of quotes list

--- src/QuotesContainer.js
+++ src/QuotesContainer.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import QuotesList from "./QuotesList";
 import QuoteForm from "./QuoteForm";
 const QuotesContainer = () => {
+  const [quotes, setQuotes] = useState([]);
   return (
     <div>
       <h1>QuotesContainer</h1>
-      <QuotesList />
+      <QuotesList quotes={quotes} />
       <QuoteForm />
     </div>
   );
--- src/QuotesList.js
+++ src/QuotesList.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-const QuotesList = () => {
+const QuotesList = (props) => {
+  const { quotes } = props;
   return (
     <div>
       <h1>Quotes List</h1>
+      {quotes.length === 0 ? (
+        <>
+          <h1>No quotes found</h1>
+          <h2>Add your first quote</h2>
+        </>
+      ) : (
+        <>
+          <h1>MyQuotes -{quotes.length}</h1>
+        </>
+      )}
     </div>
   );
 };
