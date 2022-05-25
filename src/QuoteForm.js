4e26584cac8113db8d13f100629d6c62fa330fab
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2022-05-25 01:43:49 -0400

    created form elements and callback functions to trigger the input values and stored in a formData

--- src/QuoteForm.js
+++ src/QuoteForm.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const QuoteForm = () => {
+  const [name, setName] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      id: Number(new Date()),
+      name: name,
+      body: body,
+    };
+    console.log(formData);
+  };
+
+  const handleNameChange = (e) => {
+    const result = e.target.value;
+
+    setName(result);
+  };
+
+  const handleBodyChange = (e) => {
+    const result = e.target.value;
+    setBody(result);
+  };
+
   return (
     <div>
       <h1>QuoteForm</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" onChange={handleNameChange} />
+        <br />
+        <textarea placeholder="body" onChange={handleBodyChange} />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 };
